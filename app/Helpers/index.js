814806afcf90ff3240132a827ee020d135cd1add
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-12-06 16:57:08 -0300

    Helper manage single file

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -26,6 +26,29 @@ const str_random = async (length = 40) => {
   return string
 }
 
+/**
+ * Move um único arquivo para o caminho especificado, se nenhum for especificado
+ * então 'public/uploads' será utilizado.
+ *
+ * @param { FileJar } file - O arquivo a ser gerenciado
+ * @param { string } path - O caminho para onde o arquivo deve ser movido
+ * @return { string } - Retorna o arquivo no destino final
+ */
+const manage_single_upload = async (file, path = null) => {
+  path = path ? path : Helpers.publicPath('uploads')
+
+  // Gera nome aleatório
+  const random_name = await str_random(30)
+  let filename = `${new Date().getTime()}-${random_name}.${file.subtype}`
+
+  // Renomeia arquivo
+  await file.move(path, {
+    name: filename
+  })
+
+  return file
+}
+
 module.exports = {
   str_random
 }
