d6f363943f1d57ec0e03a07cb5da655aace9965a
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-12-07 19:46:14 -0300

    Delete image

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -6,6 +6,7 @@
 
 const Image = use('App/Models/Image')
 const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers')
+const fs = use('fs')
 
 /**
  * Resourceful controller for interacting with images
@@ -145,7 +146,24 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: { id }, request, response }) {
+    const image = await Image.findOrFail(id)
+
+    try {
+      let filePath = Helpers.publicPath(`uploads/${image.path}`)
+
+      await fs.unlink(filePath, err => {
+        if (!err) {
+          await image.delete()
+        }
+      })
+
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(400).send({
+        message: "Não foi possível deletar a imagem"
+      })
+    }
   }
 }
 
