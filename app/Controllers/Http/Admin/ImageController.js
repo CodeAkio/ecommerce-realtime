91160d3f816628c3ed4fa61985053bd70bf4b3a8
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-12-07 15:45:39 -0300

    Upload image(s)

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -4,6 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Image = use('App/Models/Image')
+const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers')
+
 /**
  * Resourceful controller for interacting with images
  */
@@ -15,21 +18,15 @@ class ImageController {
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
-   * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
-  }
+  async index ({ request, response, pagination }) {
+    const images = await Image
+      .query()
+      .orderBy('id', 'desc')
+      .paginate(pagination.page, pagination.limit)
 
-  /**
-   * Render a form to be used for creating a new image.
-   * GET images/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+    return response.send(images)
   }
 
   /**
@@ -41,6 +38,63 @@ class ImageController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    try {
+      const fileJar = request.file('images', {
+        types: ['image'],
+        size: '2mb'
+      })
+
+      let images = []
+
+      if (!fileJar.files) {
+        // Upload de uma única imagem
+        const file = await manage_single_upload(fileJar)
+
+        if (file.moved()) {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+          images.push(image)
+
+          return response.status(201).send({
+            successes: images,
+            errors: {}
+          })
+        }
+
+        return response.status(400).send({
+          message: "Não foi possível processar esta imagem"
+        })
+      }
+        // Upload de várias imagens
+      let files = await manage_multiple_uploads(fileJar)
+
+      await Promise.all(
+        files.successes.map(async file => {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+          images.push(image)
+        })
+      )
+
+      return response.status(201).send({
+        successes: images,
+        errors: {}
+      })
+    } catch (error) {
+      return response.status(400).send({
+        message: "Não foi prossível salvar a(s) imagem(ns)"
+      })
+    }
   }
 
   /**
@@ -53,18 +107,6 @@ class ImageController {
    * @param {View} ctx.view
    */
   async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing image.
-   * GET images/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
   }
 
   /**
