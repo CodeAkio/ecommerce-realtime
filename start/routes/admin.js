5d4643621455aec9fffecfb6328952c4a2e6c5b2
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-12-10 17:18:50 -0300

    Discounts routes

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -10,6 +10,8 @@ Route.group(() => {
   Route.resource('orders', 'OrderController').apiOnly()
   Route.resource('images', 'ImageController').apiOnly()
   Route.resource('users', 'UserController').apiOnly()
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
 })
   .prefix('v1/admin')
   .namespace('Admin')
