544fd105106dee89b41d3a611b02d567ff46ff5a
CodeAkio <siqueira_2004@hotmail.com>
2020-12-16 13:09:20 -0300

    Add default values to pagination middleware

--- app/Middleware/Pagination.js
+++ app/Middleware/Pagination.js
@@ -11,8 +11,13 @@ class Pagination {
    */
   async handle (ctx, next) {
     if(ctx.request.method() === 'GET') {
-      const page = parseInt(ctx.request.input('page'))
-      const limit = parseInt(ctx.request.input('limit'))
+      const page = ctx.request.input('page')
+        ? parseInt(ctx.request.input('page'))
+        : 1
+
+      const limit = ctx.request.input('limit')
+        ? parseInt(ctx.request.input('limit'))
+        : 20
 
       ctx.pagination = { page, limit }
 
